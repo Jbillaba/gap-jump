e5ec8d0e1214840678687fcae46be0573eb6e074
Jeremy <jeremyvillalva66@gmail.com>
2022-10-17 09:40:33 -0400

    somehow fixed the movement, idk how but i'll take it

--- game.js
+++ game.js
@@ -1,11 +1,9 @@
 
 let block;
-//in case it wasnt obvious it makes game start....I think
 function startGame() {
     myGameArea.start();
-    block = new component(100, 30, "red", 10, 120)
+    block = new GameObject(100, 30, "red", 10, 120)
 }
-
 // makes block move + clears the canvas
 let myGameArea = {
     canvas: document.querySelector(".game-display"),
@@ -14,23 +12,20 @@ let myGameArea = {
         this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
         this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('keydown', function (e) {
-            myGameArea.keys = (myGameArea.keys || []);
-            myGameArea.keys[e.keycode] = true;
-        })
-        window.addEventListener('keyup', function (e) {
-            myGameArea.key[e.keycode] = false;
-        })
-
+    window.addEventListener('keydown', function (e) {
+        myGameArea.key = e.keyCode;
+    })    
+    window.addEventListener('keyup', function(e) {
+        myGameArea.key = false;
+    })
     }, 
     clear: function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
-
 }
 
 // gameObj class contructor
-function component(width, height, color, x, y) {
+function GameObject(width, height, color, x, y) {
     this.gameArea = myGameArea;
     this.width = width;
     this.height = height;
@@ -54,10 +49,10 @@ function updateGameArea() {
     myGameArea.clear();  
     block.speedX = 0;
     block.speedY = 0;
-    if (myGameArea.keys && myGameArea.keys == [37]) {block.speedX = -4; }
-    if (myGameArea.keys && myGameArea.keys == [39]) {block.speedX = 4; }
-    if (myGameArea.keys && myGameArea.keys == [38]) {block.speedY = -4; }
-    if (myGameArea.keys && myGameArea.keys == [40]) {block.speedY = 4; }
+    if (myGameArea.key && myGameArea.key == 37){block.speedX = -1;}
+    if (myGameArea.key && myGameArea.key == 39){block.speedX = 1;}
+    if (myGameArea.key && myGameArea.key == 38){block.speedY = -1;}
+    if (myGameArea.key && myGameArea.key == 40){block.speedY = 1;}
     block.newPos();
     block.update();
 }
