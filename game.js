f1e6ff09c903195e0f13927ea2344aea711751df
Jeremy <jeremyvillalva66@gmail.com>
2022-10-19 13:06:55 -0400

    added some stuff

--- game.js
+++ game.js
@@ -1,10 +1,13 @@
 
 let block;
 let pipes = [];
+let score;
 
 
 function startGame() {
-    block = new GameObject(30, 30, "red", 10, 120);
+    block = new GameObject(30, 30, "flappy-bird.png", 10, 120, "image");
+    score = new GameObject("30px", "silkScreen", "black", 280, 40, "text") 
+    background = new GameObject(656, 270, "sky-background.webp", 0, 0, "image");
     myGameArea.start();
 }
 // makes block move + clears the canvas
@@ -12,7 +15,7 @@ let myGameArea = {
     canvas: document.querySelector(".game-display"),
     start : function() {
         this.canvas.width = 500;
-        this.canvas.height = 600;
+        this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
@@ -33,7 +36,13 @@ let myGameArea = {
 }
 
 // gameObj class contructor
-function GameObject(width, height, color, x, y) {
+function GameObject(width, height, color, x, y, type) {
+    this.type = type;
+    if(type == "image"){
+        this.image = new Image();
+        this.image.src = color;
+    }
+    
     this.width = width;
     this.height = height;
     this.speedX = 0;
@@ -42,8 +51,21 @@ function GameObject(width, height, color, x, y) {
     this.y = y;
     this.update = function(){
     ctx = myGameArea.context;
+    if(type == "image"){
+        ctx.drawImage(this.image, 
+            this.x, 
+            this.y, 
+            this.width, this.height);
+     
+    }
+    if (this.type == "text"){
+        ctx.font = this.width + " " + this.height;
+        ctx.fillStyle = color;
+        ctx.fillText(this.text, this.x, this.y);
+    }else{
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
     }
     this.newPos = function(){
         this.x += this.speedX;
@@ -79,16 +101,17 @@ function updateGameArea() {
             return;
         }
     }
-  
     myGameArea.clear();  
+    background.newPos();
+    background.update();
     myGameArea.frameNo += 1; 
 if (myGameArea.frameNo == 1 || everyinterval(150)){
     x = myGameArea.canvas.width;
-   minHeight = 40;
-   maxHeight = 400;
+   minHeight = 50;
+   maxHeight = 250;
    height = Math.floor(Math.random() * (maxHeight - minHeight + 1)+ minHeight);
    minGap = 50;
-   maxGap = 400;
+   maxGap = 200;
    gap = Math.floor(Math.random() *(maxHeight-minHeight+1)+minHeight);
    pipes.push(new GameObject(30, height, "green", x, 0));
    pipes.push(new GameObject(30, x - height - gap, "green", x, height + gap));
@@ -104,6 +127,8 @@ if (myGameArea.frameNo == 1 || everyinterval(150)){
     if (myGameArea.key && myGameArea.key == 39){block.speedX = 1;}
     if (myGameArea.key && myGameArea.key == 38){block.speedY = -1;}
     if (myGameArea.key && myGameArea.key == 40){block.speedY = 1;}
+    score.text = "score: " + myGameArea.frameNo;
+    score.update();
     block.newPos();
     block.update();
 }
